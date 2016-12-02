8df15dabca661fbba2f8234a29956cecd8ecbf68
ellisMing <j0989574647@gmail.com>
2016-12-02 14:56:53 +0800

    Implement GCS Bucket   * bucket list   * bucket get   * bucket create   * bucket delete

--- gcb.py
+++ gcb.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 import gcpproject
+import gcpbucket
 
 
 def main():
     # gcpproject.list()
     # gcpproject.create('gcb-test1', 'ellis-gcb-test1')
     # gcpproject.delete('ellis-gcb-test1')
+
+    # gcpbucket.list('gcb-project')
+    # gcpbucket.get('gcb-test-bucket1')
+    # gcpbucket.create('gcb-project', 'gcb-test-bucket2')
+    gcpbucket.delete('gcb-test-bucket2')
 
 
 if __name__ == '__main__':
