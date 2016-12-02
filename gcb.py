27f0f9739a85d24629a11876a1f417265e697016
ellisMing <j0989574647@gmail.com>
2016-12-02 14:03:50 +0800

    Implement GCP project   * project list   * project create   * project delete

---
+++ gcb.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import gcpproject
+
+
+def main():
+    # gcpproject.list()
+    # gcpproject.create('gcb-test1', 'ellis-gcb-test1')
+    # gcpproject.delete('ellis-gcb-test1')
+
+
+if __name__ == '__main__':
+    main()
---
+++ gcpcredential.py
@@ -0,0 +1,11 @@
+import sys
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+def gcp_credential(api):
+    try:
+        credentials = GoogleCredentials.get_application_default()
+        service = discovery.build(api, 'v1', credentials=credentials)
+        return service
+    except:
+        sys.exit("Promise Undefined.")
