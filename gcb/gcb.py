25f9053bf47dd262623eb9357883bcac8f2999b3
ellisMing <j0989574647@gmail.com>
2016-12-05 16:34:31 +0800

    Implement Google Cloud Storeage Object operation:   * list   * upload   * delete

--- gcb/gcb.py
+++ gcb/gcb.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
+from gcp import project
 from gcp import bucket
+from gcp import object
 
 
 def main():
-    # gcpproject.list()
-    # gcpproject.create('gcb-test1', 'ellis-gcb-test1')
-    # gcpproject.delete('ellis-gcb-test1')
+    project.list()
+    # project.create('gcb-test1', 'ellis-gcb-test1')
+    # project.delete('ellis-gcb-test1')
 
-    # gcpbucket.list('gcb-project')
-    # gcpbucket.get('gcb-test-bucket1')
-    # gcpbucket.create('gcb-project', 'gcb-test-bucket2')
-    bucket.delete('gcb-test-bucket2')
+    bucket.list('gcb-project')
+    # bucket.get('gcb-test-bucket1')
+    # bucket.create('gcb-project', 'gcb-test-bucket2')
+    # bucket.delete('gcb-test-bucket2')
 
+    object.list('gcb-test-bucket1')
+    # object.upload('gcb-test-bucket1', 'gcb.py', 'gcb.py')
+    # object.delete('gcb-test-bucket1', 'gcb.py')
 
 if __name__ == '__main__':
     main()
---
+++ gcb/gcp/object.py
@@ -0,0 +1,38 @@
+import json, sys
+import credential
+
+
+def list():
+    try:
+        request = credential.gcp_credential('cloudresourcemanager')
+        response = request.projects().list().execute()
+        encodedjson = json.dumps(response['projects'], sort_keys=True, indent=4)
+        print encodedjson
+    except:
+        sys.exit("get project list fail.")
+
+
+def create(project_name, project_id):
+    try:
+        config = {
+            'project_id': project_id,
+            'name': project_name
+        }
+        request = credential.gcp_credential('cloudresourcemanager')
+        response = request.projects().create(body=config).execute()
+        print("create [%s] project success" % project_name)
+        encodedjson = json.dumps(response, sort_keys=True, indent=4)
+        print encodedjson
+    except:
+        sys.exit("create project fail.")
+
+
+def delete(project_id):
+    try:
+        request = credential.gcp_credential('cloudresourcemanager').projects()
+        response = request.delete(projectId=project_id).execute()
+        print("delete [%s] success" % project_id)
+        encodedjson = json.dumps(response, sort_keys=True, indent=4)
+        print encodedjson
+    except:
+        sys.exit("delete prject fail")
